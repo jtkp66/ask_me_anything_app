b79d86f5ec0656f6f1b5399ccffef4c564dd26a8
Jamie Pham <jtkp66@gmail.com>
2019-02-18 14:19:06 -0800

    ScaffoldednComponents, and wired up react-router to each component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import PostCreate from './posts/PostCreate';
+import PostList from './posts/PostList';
+import PostEdit from './posts/PostEdit';
+import PostDelete from './posts/PostDelete';
+import PostShow from './posts/PostShow';
+
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PostList} />
+                    <Route path="/posts/new" exact component={PostCreate} />
+                    <Route path="/posts/edit" exact component={PostEdit} />
+                    <Route path="/posts/delete" exact component={PostDelete} />
+                    <Route path="/posts/show" exact component={PostShow} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 };
 
 export default App;
