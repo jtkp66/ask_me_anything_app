f5633b77988acb93ea5bb1e2ea4989a94baaeab5
Jamie Pham <jtkp66@gmail.com>
2019-02-18 18:00:48 -0800

    Added google O auth, with login/logout buttons in header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
     return(
         <div className="ui secondary pointing menu">
             <Link to="/" className="item">
-                Home
+                Home Link
             </Link>
             <div className="right menu">
             <Link to="/" className="item">
-                All Posts
+                All Posts(Home)
             </Link>
+            <GoogleAuth />
             </div>
         </div>
     )
--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const PostList = () => {
-    return <div>PostList</div>;
+    return (
+        <div>
+            <h2>Page List</h2>
+            <Link to="/posts/new">Page Two</Link>
+            <hr />
+            <Link to="/posts/edit">Page Three</Link>
+            <hr />
+            <Link to="/posts/delete">Page Four</Link>
+            <hr />
+            <Link to="/posts/show">Page Five</Link>
+        </div>
+    )
 };
 
 export default PostList;
